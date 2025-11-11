65f3253eced820367f08949f970185a13da59ee6
Damir Fell <44857611+Damako4@users.noreply.github.com>
2025-11-11 11:27:58 -0500

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/fc-tmp/glad/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for glad")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/damir/Documents/FinalGame/cmake-build-debug/CMakeFiles/fc-tmp/glad/glad-gitupdate.cmake")
-
-endblock()
--- cmake-build-debug/CMakeFiles/fc-tmp/glfw/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing download step for glfw")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/damir/Documents/FinalGame/cmake-build-debug/CMakeFiles/fc-tmp/glfw/glfw-gitclone.cmake")
-
-endblock()
